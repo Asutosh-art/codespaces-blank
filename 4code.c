29ed569fe3929d7b8edba458e61ebe8be9a85b69
Asutosh-art <115441929+Asutosh-art@users.noreply.github.com>
2023-01-19 07:44:02 +0530

    Add files via upload

---
+++ 4code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    a = 34 ;
+    b = 6.65 ;
+    
+    printf("a + b = %d\n", a+b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+}
